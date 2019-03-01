37e4124cc5a8d99f7f14683dc83f6bd9bae45021
Nicholas Cai <ncai@berkeley.edu>
2019-03-01 03:18:47 -0800

    animate speech

--- animations.js
+++ animations.js
@@ -1,8 +1,25 @@
+// anime({
+//   targets: '.blob',
+//   scale: [0, 1],
+//   delay: function(el, index) {
+//     return index * 150
+//   },
+//   direction: 'forward',
+//   easing: 'easeInOutExpo'
+// });
+
 anime({
-  targets: 'img',
+  targets: '.speech-bubble',
+  opacity: [0, 1], 
   scale: [0, 1],
+  // translateY: -100,
   delay: function(el, index) {
-    return index * 150
+    return index * 500
   },
-  direction: 'forward'
-});
+  direction: 'forward',
+  easing: 'easeInOutExpo'
+})
+
+
+// var document.getElementById('blob__left--yellow').style.height;
+// var rect = element.getBoundingClientRect();
