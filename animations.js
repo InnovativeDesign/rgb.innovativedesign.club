084b1ecdc388e766474c7e9cda6f64b2acdbdd7a
Nicholas Cai <ncai@berkeley.edu>
2019-03-03 16:18:42 -0800

    added animation

--- animations.js
+++ animations.js
@@ -1,48 +1,48 @@
-// var hero_timeline = anime.timeline({
-//   easing: 'easeInOutExpo',
-// });
-//
-// hero_timeline
-// .add({
-//   targets: '.blob-main',
-//   scale: [0, 1],
-//   direction: 'forward',
-//   duration: 700
-// })
-// .add({
-//   targets: 'h1',
-//   scale: [0, 1],
-//   delay: function(el, index) {
-//     return index * 300
-//   },
-//   duration: 900,
-//   direction: 'forward',
-// }, '-=300')
-// .add({
-//   targets: '.speech-bubble',
-//   opacity: [0, 1],
-//   scaleY: [0, 1],
-//   delay: function(el, index) {
-//     return index * 400
-//   },
-//   direction: 'forward',
-// }, '-=700')
-// .add({
-//   targets: '.blob',
-//   scale: [0, 1],
-//   delay: function(el, index) {
-//     return index * 150
-//   },
-//   direction: 'forward',
-// }, '-=700')
-// .add({
-//   targets: '.logo-intersect',
-//   opacity: [1, 0],
-//   duration: 500,
-//   delay: 200
-// })
-// .add({
-//   targets: '.button__container button',
-//   scale: [0, 1],
-//   duration: 500,
-// })
+var hero_timeline = anime.timeline({
+  easing: 'easeInOutExpo',
+});
+
+hero_timeline
+.add({
+  targets: '.blob-main',
+  scale: [0, 1],
+  direction: 'forward',
+  duration: 700
+})
+.add({
+  targets: 'h1',
+  scale: [0, 1],
+  delay: function(el, index) {
+    return index * 300
+  },
+  duration: 900,
+  direction: 'forward',
+}, '-=300')
+.add({
+  targets: '.speech-bubble',
+  opacity: [0, 1],
+  scaleY: [0, 1],
+  delay: function(el, index) {
+    return index * 400
+  },
+  direction: 'forward',
+}, '-=700')
+.add({
+  targets: '.blob',
+  scale: [0, 1],
+  delay: function(el, index) {
+    return index * 150
+  },
+  direction: 'forward',
+}, '-=700')
+.add({
+  targets: '.logo-intersect',
+  opacity: [1, 0],
+  duration: 500,
+  delay: 200
+})
+.add({
+  targets: '.button__container button',
+  scale: [0, 1],
+  duration: 500,
+})
