2cfc70b8a9f8c8718b0209b308d5476ec77b227c
doud21 <doud@berkeley.edu>
2022-02-28 18:16:22 -0800

    rgb website

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import './App.css';
-import Banner from './img/BANNER.png'
+import Banner from './img/banner2.png'
 
 const scheduleContent = [
   {
-    name: "Welcome to RGB 2021",
+    name: "Welcome to RGB 2022",
     time: "1:00 pm",
-    desc: "Sit tight, RGB 2021 is about to begin!",
+    desc: "Sit tight, RGB 2022 is about to begin!",
   },
   {
-    name: "Erik Herrström",
-    time: "1:10 pm",
-    desc: "Formerly Brand Design Director at Spotify. Now, independent designer based in Vienna with focus on branding and art direction",
-    link: "https://erikherrstrom.com/"
-  },
-  {
-    name: "Elba Ornelas",
-    time: "2:00 pm",
-    desc: "A Guadalajara-based Senior UX designer, currently leading the team that’s getting rid of food waste in our Wizeline offices. I’m also a mentor, an occasional speaker, and a full-time advocate of naps.",
-    link: "https://dossiette.com/"
-  },
-  {
-    name: "Tala Safié",
-    time: "2:50 pm",
-    desc: "I am a graphic designer from Beirut based in New York. I currently work with AIGA Eye on Design 🤱🏽 and as an art director for The New York Times.",
-    link: "https://www.talasafie.com/"
-  },
-  {
-    name: "Office Hours",
-    time: "3:30 pm",
-    desc: "Come meet the designers and ask any questions you have!",
-  },
+    name: "TBD"
+  }
+  // {
+  //   name: "Erik Herrström",
+  //   time: "1:10 pm",
+  //   desc: "Formerly Brand Design Director at Spotify. Now, independent designer based in Vienna with focus on branding and art direction",
+  //   link: "https://erikherrstrom.com/"
+  // },
+  // {
+  //   name: "Elba Ornelas",
+  //   time: "2:00 pm",
+  //   desc: "A Guadalajara-based Senior UX designer, currently leading the team that’s getting rid of food waste in our Wizeline offices. I’m also a mentor, an occasional speaker, and a full-time advocate of naps.",
+  //   link: "https://dossiette.com/"
+  // },
+  // {
+  //   name: "Tala Safié",
+  //   time: "2:50 pm",
+  //   desc: "I am a graphic designer from Beirut based in New York. I currently work with AIGA Eye on Design 🤱🏽 and as an art director for The New York Times.",
+  //   link: "https://www.talasafie.com/"
+  // },
+  // {
+  //   name: "Office Hours",
+  //   time: "3:30 pm",
+  //   desc: "Come meet the designers and ask any questions you have!",
+  // },
 ]
 
 function Schedule({ content }) {
@@ -68,6 +71,7 @@ function Heading() {
       <div className="heading">
         {/* <h2 className="rotateRight">REACH GROW BUILD</h2> */}
         <img src={Banner} alt="BANNER" className="banner" />
+        <Content/>
         {/* <h2 className="rotateLeft">INNOVATIVE DESIGN</h2> */}
       </div>
     </>
@@ -84,8 +88,8 @@ function Content() {
           <h3 className="GROW"> - GROW - </h3>
           <h3 className="BUILD"> - BUILD</h3>
         </div>
-        <h3>3/13/21, 1-4pm PT</h3>
-        <a className="registerButton" href="https://www.eventbrite.com/e/rgb-2021-speaker-series-tickets-144309562787" target="_blank">REGISTER NOW</a>
+        <h3>3/12/22, 1-6pm @Jacobs 310</h3>
+        <a className="registerButton" href="https://l.messenger.com/l.php?u=https%3A%2F%2Fwww.eventbrite.com%2Fe%2F273943210517&h=AT08hvSM9i4J7fuvXTRzTbJ0mMGoyGhRA2w6RYDsXVth3osm80YJRqZyYq2n-f0gxJyMYBITM3KZDqz8BIWGu-HbuICD6ld3rVhYkJ59aQ-_szBGG5cqB0LdkIU7oMXFHLmkTpbKyPU" target="_blank">REGISTER NOW</a>
       </div>
     </>
   )
@@ -96,11 +100,13 @@ function App() {
     <>
       <div className="home">
         <Heading />
-        <Content />
         <div className="section2">
-          <p>RGB will be hosted on March 13th, and will provide students with the opportunity to listen to industry professionals discuss their careers. RGB 2021 will be a virtual event, and we'll be joined by speakers whose careers have taken them to Spotify, the New York Times, and beyond!</p>
-          <h3 style={{textAlign: "center", color: "#FFB91C"}}>SCHEDULE</h3>
+          <p>RGB (Reach Grow Build): A Design Speaker Series is an event hosted on March 12th in Jacobs Hall by Innovative Design, providing students with the chance to level up their design experience with industry professionals, everywhere from product design to 3d animation!</p>
+          <h3 style={{textAlign: "center", color: "#ff62ff"}}>SCHEDULE</h3>
           <Schedule content={scheduleContent} />
+          <div class="footer">
+            <a href="https://innovativedesign.club/" target="_blank">&copy; innovative design</a>
+          </div>
         </div>
       </div>
     </>
