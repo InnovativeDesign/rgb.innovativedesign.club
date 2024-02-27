38df5c783aafaef4e708d96484505906f36dd978
Melody Miao <melodymiao@berkeley.edu>
2024-02-27 13:40:36 -0800

    new schedule image and corrected title image

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main class="bg-[#FBE9CC] p-24" className="flex min-h-screen flex-col items-center justify-between">
       <div class="relative pb-4 flex flex-col items-center">
-        <div className="">
+        <div>
           <a href="your-link-url" className="top-[8%] left-[25%]">
             <Image className="relative pb-2 pointer-events-none" layout="responsive" width="500" height="500" priority src="/images/rgb_title_w_button.png" alt="rgb_title" />
           </a>
@@ -12,7 +12,7 @@ export default function Home() {
         <Image className="pb-2 pointer-events-none" layout="responsive" width="500" height="500" priority src="/images/rgb_info.png" alt="rgb_title" />
         <Image className="pb-2 pointer-events-none" layout="responsive" width="500" height="500" priority src="/images/rgb_schedule.png" alt="rgb_title" />
         <div className="self-center font-termina font-extrabold text-2xl">
-          <a href="https://linktr.ee/innodatcal">
+          <a class="text-black" href="https://linktr.ee/innodatcal">
             @innodatcal
           </a>
         </div>
